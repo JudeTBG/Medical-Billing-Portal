c4b5bd2941a8b0826e97f23bb8199b8579bbac3c
SUDIP KUNDU <sudip2003kundu@gmail.com>
2025-02-26 23:54:43 +0530

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,217 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <div id="header-sticky" className="header__area header-1">
+                <div className="container">
+                    <div className="mega__menu-wrapper p-relative">
+                        <div className="header__main">
+                            <div className="header__logo">
+                                <a href="index.html">
+                                    <div className="logo">
+                                        <img src="/src/assets/imgs/logo/logo.svg" alt="logo not found"/>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div className="mean__menu-wrapper d-none d-lg-block">
+                                <div className="main-menu">
+                                    <nav id="mobile-menu">
+                                        <ul>
+                                            <li className="has-dropdown has-mega-menu active">
+                                                <a href="javascript:void(0)">Home</a>
+                                                <ul className="mega-menu mega-menu-grid-3">
+                                                    <li>
+                                                        <div className="home__menu-item">
+                                                            <div className="home__menu-thumb">
+                                                                <img src="/src/assets/imgs/menu/menu-home-1.jpg" alt="thumb not found"/>
+                                                                    <div className="home__menu-buttons">
+                                                                        <a href="index.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                        <a href="index-one-page.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                    </div>
+                                                            </div>
+                                                            <h4 className="home__menu-title">Eye Care</h4>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div className="home__menu-item">
+                                                            <div className="home__menu-thumb">
+                                                                <img src="/src/assets/imgs/menu/menu-home-2.jpg" alt="thumb not found"/>
+                                                                    <div className="home__menu-buttons">
+                                                                        <a href="index-2.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                        <a href="index-2-one-page.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                    </div>
+                                                            </div>
+                                                            <h4 className="home__menu-title">Dental Care</h4>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div className="home__menu-item">
+                                                            <div className="home__menu-thumb">
+                                                                <img src="/src/assets/imgs/menu/menu-home-3.jpg" alt="thumb not found"/>
+                                                                    <div className="home__menu-buttons">
+                                                                        <a href="index-3.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                        <a href="index-3-one-page.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                    </div>
+                                                            </div>
+                                                            <h4 className="home__menu-title">Medical Care</h4>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div className="home__menu-item">
+                                                            <div className="home__menu-thumb">
+                                                                <img src="/src/assets/imgs/menu/menu-home-4.jpg" alt="thumb not found"/>
+                                                                    <div className="home__menu-buttons">
+                                                                        <a href="index-4.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                        <a href="index-4-one-page.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                    </div>
+                                                            </div>
+                                                            <h4 className="home__menu-title">Psycology & Counseling</h4>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div className="home__menu-item">
+                                                            <div className="home__menu-thumb">
+                                                                <img src="/src/assets/imgs/menu/menu-home-5.jpg" alt="thumb not found"/>
+                                                                    <div className="home__menu-buttons">
+                                                                        <a href="index-5.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">MULTI PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                        <a href="index-5-one-page.html" className="rr-btn">
+                                                                            <span className="btn-wrap">
+                                                                                <span className="text-one">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                                <span className="text-two">ONE PAGE <i className="fa-solid fa-plus"></i></span>
+                                                                            </span>
+                                                                        </a>
+                                                                    </div>
+                                                            </div>
+                                                            <h4 className="home__menu-title">Skin Care</h4>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li className="has-dropdown ">
+                                                <a href="javascript:void(0)">Pages</a>
+                                                <ul className="submenu">
+                                                    <li><a href="faq.html">Faq</a></li>
+                                                    <li><a href="about-us.html">About us</a></li>
+                                                    <li><a href="pricing.html">Pricing</a></li>
+                                                    <li><a href="pricing-2.html">Pricing 2</a></li>
+                                                    <li><a href="404.html">404 Page</a></li>
+                                                    <li><a href="appoinment.html">Appointment</a></li>
+                                                </ul>
+                                            </li>
+                                            <li className="has-dropdown">
+                                                <a href="service.html">Services</a>
+                                                <ul className="submenu">
+                                                    <li><a href="service.html">Service</a></li>
+                                                    <li><a href="service-2.html">Service 2</a></li>
+                                                    <li><a href="service-details.html">Services Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li className="has-dropdown">
+                                                <a href="doctor.html">Doctor</a>
+                                                <ul className="submenu">
+                                                    <li><a href="doctor.html">Doctor</a></li>
+                                                    <li><a href="doctor-2.html">Doctor 2</a></li>
+                                                    <li><a href="doctor-details.html">Doctor Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li className="has-dropdown">
+                                                <a href="portfolio.html">Portfolio</a>
+                                                <ul className="submenu">
+                                                    <li><a href="portfolio.html">Portfolio</a></li>
+                                                    <li><a href="portfolio-2.html">Portfolio 2</a></li>
+                                                    <li><a href="portfolio-details.html">Portfolio Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li className="has-dropdown">
+                                                <a href="blog.html">Blog</a>
+                                                <ul className="submenu">
+                                                    <li><a href="blog.html">Blog</a></li>
+                                                    <li><a href="blog-grid.html">Blog Grid</a></li>
+                                                    <li><a href="blog-grid-2.html">Blog Grid 2</a></li>
+                                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact.html">contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+
+                            <div className="header__right">
+                                <div className="header__action d-flex align-items-center">
+                                    <div className="header__btn-wrap d-none d-sm-inline-flex">
+                                        <a href="contact.html" className="rr-btn rr-btn__primary">
+                                            <span className="btn-wrap">
+                                                <span className="text-one">Read More <i className="fa-solid fa-plus"></i></span>
+                                                <span className="text-two">Read More <i className="fa-solid fa-plus"></i></span>
+                                            </span>
+                                        </a>
+                                    </div>
+
+                                    <div className="header__hamburger ml-20 d-xl-none">
+                                        <div className="sidebar__toggle">
+                                            <a className="bar-icon" href="javascript:void(0)">
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
