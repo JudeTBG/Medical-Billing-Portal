9a03025ad889cc3fa07fa8dca612c3989ec43835
SUDIP KUNDU <sudip2003kundu@gmail.com>
2025-03-04 00:18:32 +0530

    try to fixing production issues

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React,{ ReactNode } from "react";
-import Header from "../components/header";
+import Header from "../components/Header";
 import Footer from "../components/footer";
 
 interface Props {
