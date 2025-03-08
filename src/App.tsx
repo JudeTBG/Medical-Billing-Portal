c5f1b450efd895e94a65fe7e236cb30eb430345e
Mukesh015 <rg37090@gmail.com>
2025-03-08 13:12:56 +0530

    page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import React from 'react'
-import {  RouteObject, useRoutes } from "react-router-dom";
+import { RouteObject, useRoutes } from "react-router-dom";
 import './App.css'
 import HomePage from './pages/home/HomePage';
+import AboutPage from './pages/about/AboutPage';
+import ContactPage from './pages/contact/ContactPage';
+import FaqPage from './pages/faq/FaqPage';
 import PageNotfound from './pages/errors/PageNotfound';
 
 const App: React.FC = () => {
-  
-    const routes: RouteObject[] = [
-      {
-        path: "/",
-        element: (
-            <HomePage />
-        ),
-      },
-      {
-        path: "*",
-        element: <PageNotfound />,
-      },
 
-    ]
-    return useRoutes(routes);
+  const routes: RouteObject[] = [
+    {
+      path: "/",
+      element: (
+        <HomePage />
+      ),
+    },
+    {
+      path: "/about",
+      element: (
+        <AboutPage />
+      ),
+    },
+    {
+      path: "/contact",
+      element: (
+        <ContactPage />
+      ),
+    },
+    {
+      path: "/faq",
+      element: (
+        <FaqPage />
+      ),
+    },
+    {
+      path: "*",
+      element: <PageNotfound />,
+    },
+
+  ]
+  return useRoutes(routes);
 }
 
 export default App
