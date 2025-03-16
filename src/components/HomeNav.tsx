bbe50d19cdc3fa2872e7c6bf91ac277d39b1b7fc
Mukesh015 <rg37090@gmail.com>
2025-03-16 13:06:19 +0530

    nav added

--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
+import MobileNav from "./MobileNav";
 
 const Header: React.FC = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
+  const closeMobileNav = () => {
+    setIsOpened(false);
+  }
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,73 +24,76 @@ const Header: React.FC = () => {
   }, []);
 
   return (
-    <header>
-      <div
-        id="header-sticky"
-        style={{
-          backgroundColor: isScrolled ? "white" : "transparent",
-          transition: "background-color 0.3s ease",
-        }}
-        className="header__area header-1"
-      >
-        <div className="container">
-          <div className="mega__menu-wrapper p-relative">
-            <div className="header__main">
-              <div className="header__logo">
-                <a href="/">
-                  <div className="logo">
-                    <img
-                      src="/assets/imgs/logo/jude-logo.svg"
-                      alt="logo not found"
-                      height={50}
-                    />
-                  </div>
-                </a>
-              </div>
-
-              <div className="mean__menu-wrapper d-none d-lg-block">
-                <div className="main-menu">
-                  <nav id="mobile-menu">
-                    <ul>
-                      <li className=" has-mega-menu active">
-                        <a href="/">Home</a>
-                      </li>
-                      <li>
-                        <a href="/about">About</a>
-                      </li>
-                      <li>
-                        <a href="/contact">Contact</a>
-                      </li>
-                      {/* <li>
-                                                <a href="/faq">FAQ</a>
-                                            </li> */}
-                    </ul>
-                  </nav>
+    <>
+      {isOpened && <MobileNav closeMobileNav = {closeMobileNav} />}
+      <header>
+        <div
+          id="header-sticky"
+          style={{
+            backgroundColor: isScrolled ? "white" : "transparent",
+            transition: "background-color 0.3s ease",
+          }}
+          className="header__area header-1"
+        >
+          <div className="container">
+            <div className="mega__menu-wrapper p-relative">
+              <div className="header__main">
+                <div className="header__logo">
+                  <a href="/">
+                    <div className="logo">
+                      <img
+                        src="/assets/imgs/logo/jude-logo.svg"
+                        alt="logo not found"
+                        height={50}
+                      />
+                    </div>
+                  </a>
                 </div>
-              </div>
 
-              <div className="header__right">
-                <div className="header__action d-flex align-items-center">
-                  <div className="header__btn-wrap d-none d-sm-inline-flex">
-                    <a href="/contact" className="rr-btn rr-btn__primary">
-                      <span className="btn-wrap">
-                        <span className="text-one">
-                          Read More <i className="fa-solid fa-plus"></i>
-                        </span>
-                        <span className="text-two">
-                          Read More <i className="fa-solid fa-plus"></i>
-                        </span>
-                      </span>
-                    </a>
+                <div className="mean__menu-wrapper d-none d-lg-block">
+                  <div className="main-menu">
+                    <nav id="mobile-menu">
+                      <ul>
+                        <li className=" has-mega-menu active">
+                          <a href="/">Home</a>
+                        </li>
+                        <li>
+                          <a href="/about">About</a>
+                        </li>
+                        <li>
+                          <a href="/contact">Contact</a>
+                        </li>
+                        <li>
+                          <a href="/testimonials">Testimonials</a>
+                        </li>
+                      </ul>
+                    </nav>
                   </div>
+                </div>
 
-                  <div className="header__hamburger ml-20 d-xl-none">
-                    <div className="sidebar__toggle">
-                      <a className="bar-icon" href="#">
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                <div className="header__right">
+                  <div className="header__action d-flex align-items-center">
+                    <div className="header__btn-wrap d-none d-sm-inline-flex">
+                      <a href="/contact" className="rr-btn rr-btn__primary">
+                        <span className="btn-wrap">
+                          <span className="text-one">
+                            Read More <i className="fa-solid fa-plus"></i>
+                          </span>
+                          <span className="text-two">
+                            Read More <i className="fa-solid fa-plus"></i>
+                          </span>
+                        </span>
                       </a>
+                    </div>
+
+                    <div className="header__hamburger ml-20 d-xl-none">
+                      <div onClick={() => setIsOpened(true)} className="sidebar__toggle">
+                        <a className="bar-icon">
+                          <span></span>
+                          <span></span>
+                          <span></span>
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -91,8 +101,8 @@ const Header: React.FC = () => {
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,61 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar: React.FC = () => {
+
+    const [isOpened, setIsOpened] = useState<boolean>(false);
+
+    const closeMobileNav = () => {
+        setIsOpened(false);
+    };
+
     return (
         <>
+            {isOpened && <MobileNav closeMobileNav={closeMobileNav} />}
+
             {/* <!-- Backtotop start --> */}
             <div id="scroll-percentage">
                 <span id="scroll-percentage-value" data-default-color="var(--rr-color-900)" data-scroll-color="var(--rr-theme-primary)"></span>
             </div>
-            {/* <!-- Backtotop end --> */}
-
-            {/* <!-- Offcanvas area start --> */}
-            <div className="fix">
-                <div className="offcanvas__area">
-                    <div className="offcanvas__wrapper">
-                        <div className="offcanvas__content">
-                            <div className="offcanvas__top d-flex justify-content-between align-items-center">
-                                <div className="offcanvas__logo">
-                                    <a href="/">
-                                        <img src="/assets/imgs/logo/jude-logo.svg" alt="logo not found" />
-                                    </a>
-                                </div>
-                                <div className="offcanvas__close">
-                                    <button className="offcanvas-close-icon animation--flip">
-                                        <span className="offcanvas-m-lines">
-                                            <span className="offcanvas-m-line line--1"></span><span className="offcanvas-m-line line--2"></span><span className="offcanvas-m-line line--3"></span>
-                                        </span>
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="mobile-menu fix"></div>
-                            <div className="offcanvas__social">
-                                <h4 className="offcanvas__title mb-20">Subscribe & Follow</h4>
-                                <p className="mb-30">Medical practices evolved over millennia, from ancient civilizations like Egypt and Mesopotamia to the groundbreaking</p>
-                                <ul className="header-top-socail-menu d-flex">
-                                    <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="https://twitter.com/"><i className="fab fa-twitter"></i></a></li>
-                                    <li><a href="https://www.pinterest.com/"><i className="fa-brands fa-pinterest-p"></i></a></li>
-                                    <li><a href="https://vimeo.com/"><i className="fa-brands fa-vimeo-v"></i></a></li>
-                                </ul>
-                            </div>
-                            <div className="offcanvas__btn d-sm-none">
-                                <div className="header__btn-wrap">
-                                    <a href="https://themeforest.net/user/rrdevs/portfolio" className="rr-btn rr-btn__theme rr-btn__theme-white mt-40 mt-sm-35 mt-xs-30">
-                                        <span className="btn-wrap">
-                                            <span className="text-one">Purchase Now</span>
-                                            <span className="text-two">Purchase Now</span>
-                                        </span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="offcanvas__overlay"></div>
-            <div className="offcanvas__overlay-white"></div>
-            {/* <!-- Offcanvas area start --> */}
 
             {/* <!-- Header area start --> */}
             <header>
@@ -103,7 +64,7 @@ const Navbar: React.FC = () => {
                                                 <li><a href="/">Home</a></li>
                                                 <li><a href="/contact">contact</a></li>
                                                 <li><a href="/about">About</a></li>
-                                                {/* <li><a href="/faq">FAQ</a></li> */}
+                                                <li><a href="/testimonials">Testimonials</a></li>
                                             </ul>
                                         </nav>
                                     </div>
@@ -120,7 +81,7 @@ const Navbar: React.FC = () => {
                                         </div>
 
                                         <div className="header__hamburger">
-                                            <div className="sidebar__toggle">
+                                            <div onClick={() => setIsOpened(true)} className="sidebar__toggle">
                                                 <a className="bar-icon" href="javascript:void(0)">
                                                     <span></span>
                                                     <span></span>
